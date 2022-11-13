842b5f8dab131a6b4e55761903c041bf6f513f82
Brandon Dalewalker <brandonmdalewalker@gmail.com>
2022-11-13 12:17:12 -0600

    working on game

--- game.cpp
+++ game.cpp
@@ -1,4 +1,4 @@
-<include iostream>
+#include <iostream>
 using namespace std;
 
 class Being {
@@ -9,8 +9,44 @@ public:
 
 };
 
+class partymembers: public Being{
+
+};
 
 int main() {
+	cout << "The story begins in the small fishing village near the coast of Nubera." << endl;
+	cout << "You open you eyes and find yourself laying in a bed" << endl;
+	cout << "In your daze, you seek to recall your name." << endl << "What is your name?" << endl;
+	partymembers maincharacter;
+	cin >> maincharacter.name;
+	cout << "\"Ah thanks right, my name is " << maincharacter.name << "\"" << endl;
+	cout << maincharacter.name << " awakens in a small cottage in a daze with no recollection of how they got there." << endl;
+	cout << "Upon a quick glance around the room" << maincharacter.name << " realizes they are alone inside of what seems to be a cottage." << endl;
 
+	decision1:
+	cout << "What will you do?" << endl;
+	cout << "1. Get up and leave the cottage." << endl;
+	cout << "2.Take a closer look at your surroundings." << endl;
+	int decision;
+	cin >> decision;
+	if (decision == 1) {
+		cout << "You walk outside of the small one room cottage and flinch at the smell around you." << endl;
+	}
+	else if (decision == 2) {
+		cout << "You look around the room with a closer inspection and notice your room is dirty." << endl;
+		cout << "\"I really should clean this place up\" you think to yourself and you head for the door" << endl;
+	}
+	else {
+		cout << "That is not a valid entry, please enter a valid entry" << endl;
+		goto decision1;
+
+	}
+	cout << "Scenery: As you look around you see the buildings destroyed and covered in a foul smelling viscous" << endl;
+	cout << "substance.There is nothing to indicate that people still live here or may still be here." << endl;
+	cout << " You look to the ocean and see something shining into your eyes on the beach." << endl;
+	cout << "What will you do?" << endl;
+	cout << "1. Go to the beach and investigate the shining object" << endl;
+	cout << "2. Ignore the object and make your way to the nearest city" << endl;
+	cout << "3. Go into the nearby woods" << endl;
 	return 0;
 }
